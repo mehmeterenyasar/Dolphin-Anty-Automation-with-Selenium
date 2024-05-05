a22d5d5b25ab543acf758b213bd4b8a4e7699030
mehmeterenyasar <63977893+mehmeterenyasar@users.noreply.github.com>
2024-05-05 04:05:12 +0300

    Add files via upload

---
+++ open_profile.py
@@ -0,0 +1,39 @@
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service 
+import requests
+
+
+login_url = 'http://localhost:3001/v1.0/auth/login-with-token'
+auth_token = '' # Your API Token
+request_data = {
+    'token':auth_token
+}
+headers = {
+    'Content-Type': 'application/json'
+}
+
+profile_id = '' # Profile id of that you want to open
+
+response = requests.post(login_url, json=request_data, headers=headers)
+if response.status_code == 200: # Profile opened successfully
+
+    req_url = 'http://localhost:3001/v1.0/browser_profiles/'+  profile_id  +'/start?automation=1'
+    response = requests.get(req_url)
+    response_json = response.json()
+    port = str(response_json['automation']['port'])
+    chrome_drive_path = Service("C:/Users/HP/Desktop/adbot/include/chromedriver.exe")
+
+    options = webdriver.ChromeOptions()
+    options.debugger_address = '127.0.0.1:' + port
+
+    driver = webdriver.Chrome(service=chrome_drive_path, options=options)
+
+
+
+
+
+else: # Profile was not opened
+    print('Error:', response.status_code)
+
+
+
